519020a865810999d7e2978f2fa41a3ddd729b6a
Takamiyagi <n20013@std.it-college.ac.jp>
2021-02-18 07:00:05 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import './App.css'
 import ReactAudioPlayer from 'react-audio-player'
-// import QuestionCount from '../components/QuestionCount'
-import { Button } from '@material-ui/core'
-
-class Quiz extends React.Component {
+import { IconButton, Grid } from '@material-ui/core'
+import StopIcon from '@material-ui/icons/Stop'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       musics: null,
-      counter: 0,
-      total: 0,
-      result: null
+      isPlaying: false,
+      autoPlay: true
     }
+    this.handlePlay = this.handlePlay.bind(this)
+    this.handlePause = this.handlePause.bind(this)
     this.URI =
       'https://ia803209.us.archive.org/10/items/JsonSaSlicicama/classical-music-json.json'
   }
@@ -25,57 +27,39 @@ class Quiz extends React.Component {
     this.setState({ musics: data })
   }
 
-  handleClick = (answer, random) => {
-    const correct = console.log('Correct!')
-    const incorrect = console.log('Yor failed..')
-    if (answer === this.state.musics[random].title) {
-      return correct
-    } else {
-      return incorrect
-    }
+  handlePause () {
+    this.setState({ autoPlay: false })
   }
 
-  shuffle ([...answers]) {
-    for (let i = answers.length - 1; i >= 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      ;[answers[i], answers[j]] = [answers[j], answers[i]]
-    }
-    return answers
+  handlePlay () {
+    this.setState({ autoPlay: true })
   }
 
   render () {
     if (!this.state.musics) return <div />
     const random = Math.floor(Math.random() * this.state.musics.length)
-    const dummy1 = Math.floor(Math.random() * this.state.musics.length)
-    const dummy2 = Math.floor(Math.random() * this.state.musics.length)
-    const dummy3 = Math.floor(Math.random() * this.state.musics.length)
-    const answers = [random, dummy1, dummy2, dummy3]
     const url = this.state.musics[random].url
-    const t0 = this.state.musics[answers[0]].title
-    const t1 = this.state.musics[answers[0]].title
-    const t2 = this.state.musics[answers[0]].title
-    const t3 = this.state.musics[answers[0]].title
-    const rd = this.state.musics[random].title
+    const title = this.state.musics[random].title
+    const img = this.state.musics[random].image_url
+    let button
+    if (this.state.autoPlay) {
+      button = <PlayArrowIcon onClick={this.handlePlay} />
+    } else {
+      button = <StopIcon onClick={this.handlePause} />
+    }
     return (
-      <>
-        <h1>What is the music?</h1>
-        <shuffle {...answers} />
-        <ReactAudioPlayer src={url} controls />
-        <Button onClick={this.handleClick({ t0, rd })}>
-          {this.state.musics[answers[0]].title}
-        </Button>
-        <Button onClick={this.handleClick({ t1, rd })}>{t1}</Button>
-        <Button onClick={this.handleClick({ t2, rd })}>
-          {this.state.musics[answers[2]].title}
-        </Button>
-        <Button onClick={this.handleClick({ t3, rd })}>
-          {this.state.musics[answers[3]].title}
-        </Button>
-      </>
+      <div class='center'>
+        <h1>enjoy classcal music!</h1>
+        <ReactAudioPlayer src={url} autoPlay />
+        <Grid container direction='row' alignItems='center' class='mui'>
+          <IconButton aria-label='Play/pause'>{button}</IconButton>
+          <h2>title</h2>
+          <p>{title}</p>
+          <img src={img} alt='music_image' />
+        </Grid>
+      </div>
     )
   }
 }
 
-// <QuestionCount counter={props.questionId} total={props.questionTotal} />
-
-export default Quiz
+export default App
